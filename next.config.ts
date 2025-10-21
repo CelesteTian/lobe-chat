9d97d31c8301ab1451c33c50c18247420c999289
arvinxx <arvinx@foxmail.com>
2025-10-21 13:10:59 +0800

    try to fix

--- next.config.ts
+++ next.config.ts
@@ -43,6 +43,7 @@ const nextConfig: NextConfig = {
     // so we need to disable it
     // refs: https://github.com/lobehub/lobe-chat/pull/7430
     serverMinification: false,
+    turbopackFileSystemCacheForDev: true,
     webVitalsAttribution: ['CLS', 'LCP'],
     webpackBuildWorker: true,
     webpackMemoryOptimizations: true,
@@ -198,7 +199,6 @@ const nextConfig: NextConfig = {
     },
   },
   reactStrictMode: true,
-
   redirects: async () => [
     {
       destination: '/sitemap-index.xml',
@@ -271,6 +271,7 @@ const nextConfig: NextConfig = {
   // when external packages in dev mode with turbopack, this config will lead to bundle error
   serverExternalPackages: isProd ? ['@electric-sql/pglite'] : undefined,
   transpilePackages: ['pdfjs-dist', 'mermaid'],
+  turbopack: {},
 
   typescript: {
     ignoreBuildErrors: true,
